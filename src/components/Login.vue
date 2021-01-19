eb5898a0b30469c1a5deec35bfa2c19a1d838fae
liangfutang <liangfu.tang@ecarx.com.cn>
2021-01-19 17:41:15 +0800

    登录页添加登录盒子

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    登录组件
+  <div class="login_containter">
+    <div class="login_box">
+        登录盒子
+    </div>
   </div>
 </template>
 
@@ -9,5 +11,19 @@ export default { }
 </script>
 
 <style lang="less" scoped>
+.login_containter{
+    background-color: #2b4b6b;
+    height: 100%;
+}
 
+.login_box{
+    width: 450px;
+    height: 300px;
+    left: 50%;
+    top: 50%;
+    background-color: #fff;
+    border-radius: 12px;
+    position: absolute;
+    transform: translate(-50%, -50%);
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
+// 导入全局样式
+import '../assets/css/global.css'
 
 Vue.use(VueRouter)
 
