a05fd83981e883c8315e72b03d3efde103fd4994
liangfutang <liangfu.tang@ecarx.com.cn>
2021-01-26 11:21:18 +0800

    导航页的头部显示修改

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,11 @@
 <template>
     <el-container class="home-container">
       <el-header>
-        <div class="loginOutButton">
-          <el-button type="info" @click="loginOut">登出</el-button>
+        <div>
+          <img src="../assets/home-header.jpg" alt="电商后台系统图标" width="10%"/>
+          <span>电商后台管理系统</span>
         </div>
+        <el-button type="info" @click="loginOut">登出</el-button>
       </el-header>
       <el-container>
         <el-aside width="200px">Aside</el-aside>
@@ -32,17 +34,24 @@ export default {
 }
 
 .el-header {
-  background-color: #8f7dca;
-}
-
-.loginOutButton {
-    float: right;
-    margin-top:10px;
-    margin-right:15px;
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
 }
 
 .el-aside {
-  background-color: #4f5f5f;
+  background-color: #333744;
 }
 
 .el-main {
