cda5a568e7c8d7753566aa741a5de490f8126f52
liangfutang <liangfu.tang@ecarx.com.cn>
2021-01-27 10:52:43 +0800

    修改左侧导航栏

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,14 +11,9 @@
       <el-container>
         <!-- 左侧导航栏 -->
         <el-aside width="200px">
-          <el-menu
-            default-active="2"
-            class="el-menu-vertical-demo"
-            @open="handleOpen"
-            @close="handleClose"
-            background-color="#333744"
-            text-color="#fff"
-            active-text-color="#ffd04b">
+          <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen"
+            @close="handleClose" background-color="#333744" text-color="#fff" active-text-color="#ffd04b">
+            <!-- 导航一 -->
             <el-submenu index="1">
               <template slot="title">
                 <i class="el-icon-location"></i>
@@ -26,8 +21,8 @@
               </template>
               <el-menu-item-group>
                 <template slot="title">分组一</template>
-                <el-menu-item index="1-1"> 选项1</el-menu-item>
-                <el-menu-item index="1-2"> 选项2</el-menu-item>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
               </el-menu-item-group>
               <el-menu-item-group title="分组2">
                 <el-menu-item index="1-3">选项3</el-menu-item>
@@ -37,14 +32,17 @@
                 <el-menu-item index="1-4-1">选项1</el-menu-item>
               </el-submenu>
             </el-submenu>
+            <!-- 导航二，下面没有了，所以不用子菜单 -->
             <el-menu-item index="2">
               <i class="el-icon-menu"></i>
               <span slot="title">导航二</span>
             </el-menu-item>
+            <!-- 导航三，下面没有了，所以不用子菜单 -->
             <el-menu-item index="3" disabled>
               <i class="el-icon-document"></i>
               <span slot="title">导航三</span>
             </el-menu-item>
+            <!-- 导航四，下面没有了，所以不用子菜单 -->
             <el-menu-item index="4">
               <i class="el-icon-setting"></i>
               <span slot="title">导航四</span>
